f34dcd94b1932564d97fe1a96fc69f85e13653e2
Andr0human <ayushsinha132001@gmail.com>
2022-10-12 22:35:14 +0530

    Code refactored.

--- Project Euler(HackkerRank)/19.cpp
+++ Project Euler(HackkerRank)/19.cpp
@@ -11,97 +11,99 @@ using namespace std;
 
 #define yes "Yes"
 #define no "No"
-#define ll long long
 #define ull unsigned long long
 const int mod = (int)1e9 + 7;
 
-struct day_date {
-    int day;
+struct day_date
+{
     int date;
     int month;
-    ull year;
+    uint64_t year;
+    int day;
 
     day_date() {}
 
-    day_date(int dt, int m, ull y, int d) {
-        date = dt;
-        month = m;
-        year = y;
-        day = d;
-    }
+    day_date(int dt, int m, ull y, int d)
+    : date(dt), month(m), year(y), day(d) {}
 
-    void get() {
-        cin >> year >> month >> date;
-    }
+    void
+    get()
+    { cin >> year >> month >> date; }
 
-    bool is_same(day_date &d) {
-        if (d.date == date && d.month == month && d.year == year)
-            return true;
-        return false;
-    }
+    bool
+    is_same(const day_date &d) const
+    { return d.date == date and d.month == month and d.year == year; }
 };
 
-class Calendar {
-
-    int month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    string days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    string name[13] = {"", "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"};
-    const ull per_shift_count = 688;
-
-
+class Calendar
+{
+  private:
+    const int month[13] = {
+        0, 31, 28, 31, 30, 31,
+        30, 31, 31, 30, 31, 30, 31
+    };
+    const string days[7] = {
+        "Sunday", "Monday", "Tuesday",
+        "Wednesday", "Thursday", "Friday", "Saturday"
+    };
+    string name[13] = {
+        "", "January", "February", "March",
+        "April", "May", "June", "July", "August",
+        "September", "October", "November", "December"
+    };
+    
+    const static ull per_shift_count = 688;
     day_date start_day;
 
-    bool is_leap_year(ull year) {
-        if (year % 4 == 0 && year % 100 != 0) return true;
-        if (year % 400 == 0) return true;
-        return false;
-    }
+    bool
+    is_leap_year(ull year) const
+    { return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0); }
 
-    void print(day_date &d) {
-        std::cout << d.date << " " << name[d.month] << " " << d.year << ", " << days[d.day] << std::endl;
-    }
+    void
+    print(const day_date &d) const
+    { cout << d.date << " " << name[d.month] << " " << d.year << ", " << days[d.day] << endl; }
 
-    public:
+  public:
+    Calendar() :
+    start_day(1, 1, 1900, 1) {}
 
-    Calendar() {
-        start_day = day_date(1, 1, 1900, 1);
-    }
-
-    void next(day_date &d) {
+    void
+    next(day_date d)
+    {
         int lastDate = month[d.month];
-
-        if (lastDate == 28 && is_leap_year(d.year)) lastDate++;
+        if ((lastDate == 28) and is_leap_year(d.year)) lastDate++;
 
         d.day = (d.day + 1) % 7;
         d.date++;
-        if (d.date > lastDate) {
+        if (d.date > lastDate)
+        {
             d.date = 1;
             d.month++;
         }
-        if (d.month > 12) {
+        if (d.month > 12)
+        {
             d.month = 1;
             d.year++;
         }
     }
 
-    ull shifted_years(ull start) {
-        ull val = start - 1900;
-        ull div = val / 400;
-        val = div * 400 + 1900;
-        return val;
-    }
+    ull
+    shifted_years(ull start) const
+    { return static_cast<ull>((start - 1900) / 400) * 400 + 1900; }
 
-    ull Solve(day_date start, day_date end) {
-        
+    ull
+    Solve(const day_date start, const day_date end)
+    {    
         start_day.year = shifted_years(start.year);
         day_date curr = traverse_to(start);
 
         ull counter = 0;
         if (!curr.day && curr.date == 1) counter++;
 
-        while (true) {
-            if (curr.is_same(end)) break;
+        while (true)
+        {
+            if (curr.is_same(end))
+                break;
             next(curr);
             if (!curr.day && curr.date == 1)
                 counter++;
@@ -110,10 +112,13 @@ class Calendar {
         return counter;
     }
 
-    day_date traverse_to(day_date end) {
+    day_date
+    traverse_to(const day_date end)
+    {
         day_date known = start_day;
         int counter = 0;
-        while (true) {
+        while (true)
+        {
             if (known.is_same(end)) break;
             next(known);
             if (!known.day && known.date == 1)
@@ -124,16 +129,18 @@ class Calendar {
 };
 
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-
-    int testcases; cin >> testcases;
-    while(testcases--) {
+int main()
+{
+    int testcases;
+    cin >> testcases;
+    
+    while(testcases--)
+    {
         Calendar cal;
         day_date d1, d2;
         d1.get();
         d2.get();
         ull x = cal.Solve(d1, d2);
-        std::cout << x << std::endl;
+        cout << x << endl;
     }
 }
--- Project Euler(HackkerRank)/26.cpp
+++ Project Euler(HackkerRank)/26.cpp
@@ -20,58 +20,80 @@ using namespace std;
 const int mod = (int)1e9 + 7;
 const int maxN = (int)1e4;
 
-void print(vector<int>& arr) {
-    for (int i : arr) std::cout << i << " ";
-    std::cout << std::endl;
+void
+print(vector<int>& arr)
+{
+    for (int i : arr)
+        cout << i << " ";
+    cout << endl;
 }
 
-int simplify(int N) {
+int
+simplify(int N)
+{
     while (N % 2 == 0) N /= 2;
     while (N % 5 == 0) N /= 5;
     return N;
 }
 
-vector<int> generator(int N, int &num) {
-
+vector<int>
+generator(int N, int& num)
+{
     vector<int> res;
-    bool occured[N] = {0};               // Store the remainder after every division
-    while (true) {
-        while (num < N) {
+
+    // Store the remainder after every division
+    bool occured[N] = {0};
+    while (true)
+    {
+        while (num < N)
+        {
             num *= 10;
             res.emplace_back(0);
         }
         int div = num / N;
         if (res.size()) res.pop_back();
         num %= N;
-        if (occured[num]) break;        // Check if remainder already occured, if yes cycle found!
+        
+        // Check if remainder already occured, if yes cycle found!
+        if (occured[num]) break;
         occured[num] = true;
-        res.emplace_back(div);          // Can be removed for speed, but is useful if we want to see the cycle and still fast enough
+
+        // Can be removed for speed, but is usefull
+        // if we want to see the cycle and still fast enough
+        res.emplace_back(div);
     }
 
     return res;
 }
 
-int generate_cycle(int N) {
-
+int
+generate_cycle(int N)
+{
     int num = 1;
-    while (num < N) num *= 10;
-    vector<int> arr1, arr2;
-    arr1 = generator(N, num);                       // Cycle can be wrong because of digits before the cycle
-    arr2 = generator(N, num);                       // Correct cycle as it starts with no leading digits
+    while (num < N)
+        num *= 10;
+
+    // Cycle can be wrong because of digits before the cycle
+    const auto cycle1 = generator(N, num);
+
+    // Correct cycle as it starts with no leading digits
+    const auto cycle2 = generator(N, num);
 
     // print(arr1);
     // print(arr2);
     // Use print function to see both cycles and their diff on 48
 
-    return arr2.size();
-
+    return cycle2.size();
 }
 
-vector<int> preCompute() {
+vector<int>
+preCompute()
+{
     // Generating cycle for every number upto maxN(10'000)
 
     vector<int> arr(maxN + 1, 0);
-    for (int i = 3; i <= maxN; i++) {
+    for (int i = 3; i <= maxN; i++)
+    {
         int x = simplify(i);
         if (x > 1) arr[i] = generate_cycle(x);
     }
@@ -79,26 +101,30 @@ vector<int> preCompute() {
     return arr;
 }
 
-int mySolution(int N, vector<int> &arr) {
-
+int
+mySolution(int N, vector<int> &arr)
+{
     // O(1) solution for query exists, but O(N) also does the job!
 
     int best = 3;
-    for (int i = 3; i < N; i++) {
+    for (int i = 3; i < N; i++)
+    {
         if (arr[i] > arr[best])
             best = i;
     }
     return best;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(0); cin.tie(0);
 
     // perf::Time(preCompute);
     vector<int> cycle_list = preCompute();
     int testcases; cin >> testcases;
 
-    while(testcases--) {
+    while(testcases--)
+    {
         int N; cin >> N;
         cout << mySolution(N, cycle_list) << endl;
     }
